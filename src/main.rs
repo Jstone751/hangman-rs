e69d5a10f7c2875e67d8840422a29b9e17bae94f
Josh cmd <joshua.stoneham@gmail.com>
2024-12-06 18:41:34 -0500

    Add command structures

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,59 @@
+use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 
-#[derive(Parser, Debug, Default)]
-#[command(version, about, author)]
+#[derive(Parser)]
+#[command(version, author, about)]
 struct Cli {
-    /// Name of the user
-    name: Option<String>,
-
     #[command(subcommand)]
-    command: Option<Commands>,
+    subcommands: Commands,
 
     /// Debug level
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    #[arg(short, long, action = clap::ArgAction::SetTrue, default_value = "0")]
+    debug: bool,
+    
+    //TODO: Add a config file
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, Clone)]
 enum Commands {
-    // Does testing
-    Test {
-        // Test item
+    /// Guess a list of characters
+    Guess {
+        /// The list of characters to check
         #[arg(short, long)]
-        list: bool,
+        guess: String,
     },
+    /// Query to see if a character has already been guessed
+    /// 
+    /// If no character is provided, the entire list of guessed
+    /// characters will be returned with a status if it is correct
+    Query {
+        ///The list of characters to query
+        #[arg(short, long)]
+        check: Option<String>,
+    },
+    /// Start a new game
+    /// 
+    /// Optionally, provide a filename to select a random word from
+    /// If no filename is provided, a random word will be selected
+    /// From a wordlist online
+    New {
+        #[arg(short, long, value_name = "OPTIONAL FILE")]
+        file: Option<PathBuf>,
+    },
+    /// Save the current game from the program's internal file to a custom file
+    Save {
+        #[arg(short, long, value_name = "FILE")]
+        file: PathBuf,
+    },
+    /// Load a game from a file to program's internal file
+    Load {
+        #[arg(short, long, value_name = "FILE")]
+        file: PathBuf,
+    },
+    /// Show the current word with the guessed characters
+    Show,
 }
 
 fn main() {
     let cli = Cli::parse();
-
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {}", name);
-    }
-
-    match cli.debug {
-        0 => println!("Debug is off"),
-        1 => println!("Debug is on"),
-        2 => println!("Debug is very on"),
-        _ => println!("Don't be silly!"),
-    }
-
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing list");
-            } else {
-                println!("Not printing list");
-            }
-        }
-        None => {}
-    }
 }
