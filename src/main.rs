0b50281a0eb0f9fbc9d9b62d8a4f746bac33ebbd
Josh cmd <joshua.stoneham@gmail.com>
2024-12-08 15:24:05 -0500

    Added savefile support, fixed relative dirs bug, added word generation, savefile overriding, and implemented the 'new' function

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use fern::{
     colors::{Color, ColoredLevelConfig},
     Dispatch,
 };
-use figment::value::Value;
+use figment::value::{Dict, Map, Value};
 use figment::{
     providers::{Format, Toml},
-    Figment, Provider,
+    Error, Figment, Profile, Provider,
 };
 use log::{debug, error, info};
+use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
+use std::io::Write;
 use std::path::PathBuf;
 
 #[derive(Parser, Clone)]
@@ -78,10 +80,11 @@ pub(crate) struct Config {
     wordlist: Option<PathBuf>,
     savefile: Option<PathBuf>,
     logfile: Option<PathBuf>,
-    debug: u8,
+    strikes: u8,
 }
 
 impl Default for Config {
+    //noinspection SpellCheckingInspection
     fn default() -> Self {
         let (savefile, logfile) = if cfg!(windows) {
             (
@@ -96,15 +99,21 @@ impl Default for Config {
             )
         } else {
             (
-                PathBuf::from("~/.config/hangman_current_game.toml"),
-                PathBuf::from("~/.config/hangman.log"),
+                PathBuf::from(format!(
+                    "{}/.config/hangman_current_game.toml",
+                    std::env::var("HOME").unwrap()
+                )),
+                PathBuf::from(format!(
+                    "{}/.config/hangman.log",
+                    std::env::var("HOME").unwrap()
+                )),
             )
         };
         Config {
             wordlist: None,
             savefile: Some(savefile),
             logfile: Some(logfile),
-            debug: 0,
+            strikes: 8,
         }
     }
 }
@@ -116,7 +125,7 @@ impl Provider for Config {
 
     fn data(
         &self,
-    ) -> Result<figment::value::Map<figment::Profile, figment::value::Dict>, figment::Error> {
+    ) -> Result<Map<Profile, Dict>, Error> {
         let wordlist_conv = match &self.wordlist {
             None => "None",
             Some(pathbuf) => pathbuf.to_str().unwrap(),
@@ -129,13 +138,53 @@ impl Provider for Config {
             None => "None",
             Some(pathbuf) => pathbuf.to_str().unwrap(),
         };
-        let mut dict = figment::value::Dict::new();
+        let mut dict = Dict::new();
         dict.insert("wordlist".to_string(), Value::from(wordlist_conv));
         dict.insert("savefile".to_string(), Value::from(savefile_conv));
         dict.insert("logfile".to_string(), Value::from(logfile_conv));
-        dict.insert("debug".to_string(), Value::from(self.debug));
+        dict.insert("strikes".to_string(), Value::from(self.strikes));
         Ok(figment::value::Map::from([(
-            figment::Profile::Default,
+            Profile::Default,
+            dict,
+        )]))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct Savefile {
+    pub(crate) word: String,
+    pub(crate) guessed: Vec<char>,
+    pub(crate) correct: Vec<char>,
+    pub(crate) incorrect: Vec<char>,
+    pub(crate) strikes_left: u8,
+}
+
+impl Default for Savefile {
+    fn default() -> Self {
+        Self {
+            word: "".to_string(),
+            guessed: vec![],
+            correct: vec![],
+            incorrect: vec![],
+            strikes_left: 8,
+        }
+    }
+}
+
+impl Provider for Savefile {
+    fn metadata(&self) -> figment::Metadata {
+        figment::Metadata::named("Default savefile")
+    }
+
+    fn data(&self) -> Result<Map<Profile, Dict>, Error> {
+        let mut dict = Dict::new();
+        dict.insert("word".to_string(), Value::from(self.word.clone()));
+        dict.insert("guessed".to_string(), Value::from(self.guessed.clone()));
+        dict.insert("correct".to_string(), Value::from(self.correct.clone()));
+        dict.insert("incorrect".to_string(), Value::from(self.incorrect.clone()));
+        dict.insert("strikes_left".to_string(), Value::from(self.strikes_left));
+        Ok(figment::value::Map::from([(
+            Profile::Default,
             dict,
         )]))
     }
@@ -152,11 +201,71 @@ fn handle_query(check: Option<String>) {
     }
 }
 
-fn handle_new(file: Option<PathBuf>) {
-    match file {
-        Some(file) => println!("Starting new game with file: {:?}", file),
-        None => println!("Starting new game with random word"),
+//noinspection SpellCheckingInspection
+fn handle_new(file: Option<PathBuf>, savefile_path: PathBuf) {
+    //noinspection SpellCheckingInspection
+    let mut random_word: String;
+
+    if let Some(file_path) = file {
+        info!("Starting new game with wordfile: {:?}", file_path);
+        if !file_path.exists() {
+            error!("Wordlist file does not exist, exiting");
+            std::process::exit(1);
+        } else if file_path.is_dir() {
+            error!("Given wordlist is a directory, exiting");
+            std::process::exit(1);
+        } else {
+            let wordlist = std::fs::read_to_string(file_path)
+                .unwrap()
+                .lines()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>();
+            random_word = wordlist[thread_rng().gen_range(0..wordlist.len())].to_string();
+            debug!(
+                "Successfully generated random word from file: {}",
+                random_word
+            );
+        }
+    } else {
+        let api_response = reqwest::blocking::get("https://random-word-api.vercel.app/api?words=1")
+            .expect("Failed to get random word from api!");
+        random_word = api_response.text().unwrap();
+        debug!(
+            "Successfully generated random word from API: {}",
+            random_word
+        );
     }
+
+    random_word = random_word
+        .trim_matches(|x| x == '[' || x == ']' || x == '"')
+        .parse()
+        .unwrap();
+
+    // Load the existing savefile
+    let mut savefile: Savefile = Figment::new()
+        .merge(Toml::file(&savefile_path))
+        .extract()
+        .expect("Failed to load savefile");
+
+    // Update the word field
+    savefile.word = random_word.clone();
+    let mut file = std::fs::File::create(&savefile_path).expect("Failed to create new savefile");
+    file.write_all(
+        toml::to_string(&Savefile {
+            word: random_word,
+            guessed: vec![],
+            correct: vec![],
+            incorrect: vec![],
+            strikes_left: 8,
+        })
+        .expect("Failed to serialize savefile")
+        .as_bytes(),
+    )
+    .unwrap();
+}
+
+fn verify_toml_file(file: &PathBuf) -> bool {
+    file.exists() && file.is_file() && file.extension() == Some("toml".as_ref())
 }
 
 fn handle_save(file: PathBuf) {
@@ -209,12 +318,14 @@ fn init_logger(debug: u8) -> Result<(), fern::InitError> {
     Ok(())
 }
 
-fn verify_toml_file(file: &PathBuf) -> bool {
-    file.is_file() && file.exists() && file.extension() == Some("toml".as_ref())
-}
-
 fn main() {
     let cli = Cli::parse();
+    // Initialize the logger
+    if let Err(e) = init_logger(cli.debug) {
+        eprintln!("Failed to initialize logger: {:?}", e);
+        std::process::exit(1);
+    }
+    debug!("Successfully initialized logger");
 
     // Load configuration file if provided
     let mut using_default_config = false;
@@ -260,16 +371,26 @@ fn main() {
         info!("Loading default internal configuration");
     }
 
-    // Extract the debug level from the configuration
-    let config: Config = figment.extract().expect("Failed to extract configuration");
-    let debug_level = if cli.debug == 0 { config.debug } else { cli.debug };
-
-    // Initialize the logger
-    if let Err(e) = init_logger(debug_level) {
-        eprintln!("Failed to initialize logger: {:?}", e);
-        std::process::exit(1);
+    let savefile: PathBuf = figment
+        .extract::<Config>()
+        .expect("Failed to extract configuration")
+        .savefile
+        .unwrap_or(Config::default().savefile.unwrap());
+    debug!("Current received savefile: {:?}", savefile);
+    info!("Savefile does not exist, creating new savefile");
+    if !savefile.exists() {
+        info!("Savefile does not exist, creating new savefile");
+        if let Some(parent) = savefile.parent() {
+            std::fs::create_dir_all(parent).expect("Failed to create savefile directory");
+        }
+        let mut file = std::fs::File::create(&savefile).expect("Failed to create new savefile");
+        file.write_all(
+            toml::to_string(&Savefile::default())
+                .expect("Failed to serialize savefile")
+                .as_bytes(),
+        )
+        .expect("Failed to write savefile");
     }
-    debug!("Successfully initialized logger");
 
     match cli.subcommands {
         Commands::Guess { guess } => {
@@ -282,7 +403,7 @@ fn main() {
         }
         Commands::New { file } => {
             debug!("Running the handler for new function");
-            handle_new(file);
+            handle_new(file, savefile);
         }
         Commands::Save { file } => {
             debug!("Running the handler for save function");
